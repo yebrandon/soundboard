6eb99c3f6aa3ad8e8f202b37d918eb9fefa99fda
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2020-02-16 10:00:35 -0500

    first commit

---
+++ Assets/ExtraFeatures.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class ExtraFeatures : MonoBehaviour
+{
+    public Slider doppler;
+
+    public AudioSource musicSource;
+
+    public AudioMixerSnapshot echo;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doppler.value == 2)
+        {
+            echo.TransitionTo(.01f);
+        }
+    }
+
+}
---
+++ Assets/slider_Mega.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class slider_Mega : MonoBehaviour
+{
+    public Slider memeSound;
+
+    public Slider volume;
+
+    public Slider doppler;
+
+    public AudioSource musicSource;
+
+    public Button song;
+
+    private bool playing = false;
+
+    public AudioMixerSnapshot boost;
+    public AudioMixerSnapshot normal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = song.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        musicSource.pitch = memeSound.value;
+
+        musicSource.volume = volume.value;
+
+        if (doppler.value == 0)
+        {
+            normal.TransitionTo(.01f);
+        }
+        if(doppler.value == 1)
+        {
+            boost.TransitionTo(.01f);
+        }
+
+        
+    }
+    void TaskOnClick()
+    {
+        if (playing)
+        {
+            musicSource.Pause();
+            playing = false;
+        }
+        else
+        {
+            musicSource.Play();
+            playing = true;
+        }
+    }
+}
